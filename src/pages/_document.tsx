0818e673ae5c4e4fbe2dbd69410a7e43f4d8084b
mbacker <marius.backer@nav.no>
2022-07-07 17:20:38 +0200

    hovedlayout

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="nb">
+      <Head>
+        <meta name="description" content="Bemanningsbehov, en del av Plattform for Rekruttering og Intern Mobilitet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=optional"
+          rel="stylesheet"
+        />
+        <link
+          rel="icon"
+          href="/Users/B160814/Workspace/prim-bemanningsbehov/public/favicon.ico"
+        />
+      </Head>
+      <body>
+      <Main />
+      <NextScript />
+      </body>
+    </Html>
+  )
+}
