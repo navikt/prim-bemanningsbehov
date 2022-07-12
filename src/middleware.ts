2d9a5dd7f96818c649bd696a1ffffb0b38f1e49b
mbacker <marius.backer@nav.no>
2022-07-12 15:26:45 +0200

    sidecar auth testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
-  console.log("\t--- Received request ---")
-  console.log("pathname: ", request.nextUrl.pathname)
-  console.log(request.headers)
+  if (request.nextUrl.pathname.startsWith("/api/")) {
+    console.log("\t--- Received request ---")
+    console.log("pathname: ", request.nextUrl.pathname)
+    console.log("auth ", request.headers.get("authorization"))
+  }
 
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: "/api/debug",
 }
