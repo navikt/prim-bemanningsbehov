76640b8cc395ccce922560a56a3dab62c2673fc0
mbacker <marius.backer@nav.no>
2022-07-12 16:10:30 +0200

    formatering

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,11 @@ import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/api/")) {
-    const timestamp = Date.now().toLocaleString("nb-NO")
+    const timestamp = (new Date()).toDateString()
     console.log(`${timestamp}\t--- Received request ---`)
     console.log("pathname: ", request.nextUrl.pathname)
     for (const pair in request.headers.entries()) {
-      console.log(pair[0], " ", pair[1])
+      console.log(pair)
     }
   }
 
