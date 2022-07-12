e46de10a03f6d1403b56dcd4864bc18d82a5ebf9
mbacker <marius.backer@nav.no>
2022-07-12 15:59:49 +0200

    request debug og config tweaks

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,12 @@ import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/api/")) {
-    console.log("\t--- Received request ---")
+    const timestamp = Date.now().toLocaleString("nb-NO")
+    console.log(`${timestamp}\t--- Received request ---`)
     console.log("pathname: ", request.nextUrl.pathname)
-    console.log("auth ", request.headers.get("authorization"))
+    for (const pair in request.headers.entries()) {
+      console.log(pair[0], " ", pair[1])
+    }
   }
 
   return NextResponse.next()
